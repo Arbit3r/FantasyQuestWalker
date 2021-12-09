c7439fd2fee71866383685017707684adc0aa40e
nnyeh <emil.munne@gmail.com>
2021-12-09 16:01:48 +0200

    Revert "korjaus"
    This reverts commit c795c888

--- app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
+++ app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
@@ -3,24 +3,12 @@ package com.example.fantasyquestwalker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    Animation textAnim;
-    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        text = findViewById(R.id.quest);
-
-        new AnimationUtils();
-        textAnim = AnimationUtils.loadAnimation(this, R.anim.text_animation);
-        text.startAnimation(textAnim);
     }
 }
