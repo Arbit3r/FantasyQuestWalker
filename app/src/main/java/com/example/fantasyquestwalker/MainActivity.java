802f6051a16bec3b9ed817eda166a24b99c7a3b6
nnyeh <emil.munne@gmail.com>
2021-12-09 20:05:37 +0200

    animaatioiden alku ja menu tehty osittain

--- app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
+++ app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
@@ -3,24 +3,47 @@ package com.example.fantasyquestwalker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Animation textAnim;
+    Animation whiteAnimEnabled, whiteAnimDisabled, textAnimEnabled, textAnimDisabled, textAnimLoad;
     TextView text;
+    LinearLayout white;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        text = findViewById(R.id.quest);
+        text = findViewById(R.id.steps);
+        white = findViewById(R.id.white);
+        white.setVisibility(View.GONE);
 
         new AnimationUtils();
-        textAnim = AnimationUtils.loadAnimation(this, R.anim.text_animation);
-        text.startAnimation(textAnim);
+        whiteAnimEnabled = AnimationUtils.loadAnimation(this, R.anim.white_animation_enabled);
+        whiteAnimDisabled = AnimationUtils.loadAnimation(this, R.anim.white_animation_disabled);
+        textAnimEnabled = AnimationUtils.loadAnimation(this, R.anim.text_animation_enabled);
+        textAnimDisabled = AnimationUtils.loadAnimation(this, R.anim.text_animation_disabled);
+        textAnimLoad = AnimationUtils.loadAnimation(this, R.anim.text_animation_load);
+
+        text.startAnimation(textAnimLoad);
+
+        ToggleButton toggle = findViewById(R.id.togglebutton);
+        toggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                white.setVisibility(View.VISIBLE);
+                white.startAnimation(whiteAnimEnabled);
+                text.startAnimation(textAnimEnabled);
+            } else {
+                white.startAnimation(whiteAnimDisabled);
+                text.startAnimation(textAnimDisabled);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/fantasyquestwalker/Splash.java
@@ -0,0 +1,37 @@
+package com.example.fantasyquestwalker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH = 2500;
+
+    Animation startTextAnim;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        text = findViewById(R.id.quest);
+
+        new AnimationUtils();
+        startTextAnim = AnimationUtils.loadAnimation(this, R.anim.splash_text_animation);
+        text.startAnimation(startTextAnim);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Splash.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+            finish();
+        }, SPLASH);
+    }
+}
