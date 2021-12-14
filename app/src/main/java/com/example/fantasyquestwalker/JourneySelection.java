5890e15614c4d2c5a8574a87e181a5867a91f6a8
Ricu Ruotsinsalo <thericu@gmail.com>
2021-12-14 16:19:28 +0200

    Listview?

--- app/src/main/java/com/example/fantasyquestwalker/JourneySelection.java
+++ app/src/main/java/com/example/fantasyquestwalker/JourneySelection.java
@@ -2,12 +2,36 @@ package com.example.fantasyquestwalker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class JourneySelection extends AppCompatActivity {
+
+    public static final String EXTRA = "extra";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journeyselector);
+
+        ListView lv = findViewById(R.id.lista);
+
+        lv.setAdapter(new ArrayAdapter<Journey>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Singleton.getInstance().getMatkat())
+        );
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //add stuff here
+            }
+        });
     }
 }
