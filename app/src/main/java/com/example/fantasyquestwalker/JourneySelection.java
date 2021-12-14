c692ddf6ff93d2a31baf9d5f3c381151937683b2
Arbit3r <rikukoski2@gmail.com>
2021-12-14 07:47:51 +0200

    Backup

---
+++ app/src/main/java/com/example/fantasyquestwalker/JourneySelection.java
@@ -0,0 +1,13 @@
+package com.example.fantasyquestwalker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JourneySelection extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journeyselector);
+    }
+}
--- app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
+++ app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.fantasyquestwalker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-//testing
+
+    Animation whiteAnimEnabled, whiteAnimDisabled, textAnimEnabled, textAnimDisabled, textAnimLoad;
+    LinearLayout white, text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.textbox);
+        white = findViewById(R.id.white);
+        white.setVisibility(View.GONE);
+
+        new AnimationUtils();
+        whiteAnimEnabled = AnimationUtils.loadAnimation(this, R.anim.white_animation_enabled);
+        whiteAnimDisabled = AnimationUtils.loadAnimation(this, R.anim.white_animation_disabled);
+        textAnimEnabled = AnimationUtils.loadAnimation(this, R.anim.text_animation_enabled);
+        textAnimDisabled = AnimationUtils.loadAnimation(this, R.anim.text_animation_disabled);
+        textAnimLoad = AnimationUtils.loadAnimation(this, R.anim.text_animation_load);
+
+        text.startAnimation(textAnimLoad);
+
+        ToggleButton toggle = findViewById(R.id.togglebutton);
+        toggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                white.setVisibility(View.VISIBLE);
+                white.startAnimation(whiteAnimEnabled);
+                text.startAnimation(textAnimEnabled);
+            } else {
+                white.setVisibility(View.GONE);
+                white.startAnimation(whiteAnimDisabled);
+                text.startAnimation(textAnimDisabled);
+            }
+        });
+        Button btn = (Button)findViewById(R.id.changejourney);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, JourneySelection.class));
+            }
+        });
     }
 }
