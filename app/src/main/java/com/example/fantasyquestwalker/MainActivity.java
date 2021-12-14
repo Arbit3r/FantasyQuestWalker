70024125d1b82b2f1275819884bba56726bde306
Julius Kotipalo <julius.kotipalo@gmail.com>
2021-12-14 07:48:35 +0200

    Askellaskuri mainiin
    askellaskuri by 7r, ja main activityyn tekstikenttä askelmäärää varten.

--- app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
+++ app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
@@ -1,9 +1,23 @@
 package com.example.fantasyquestwalker;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,11 +26,19 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private double MagnitudePrevious;
+    float stepCount = 0;
+
+    private float savedStepCount;
+    private float createStepSave;
 
     Animation whiteAnimEnabled, whiteAnimDisabled, textAnimEnabled, textAnimDisabled, textAnimLoad;
     LinearLayout white, text;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,12 +68,67 @@ public class MainActivity extends AppCompatActivity {
                 text.startAnimation(textAnimDisabled);
             }
         });
-        Button btn = (Button)findViewById(R.id.changejourney);
+        Button btn = (Button) findViewById(R.id.changejourney);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, JourneySelection.class));
             }
         });
+
+
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.jalka);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 101);
+        } else {
+            Log.d("sensor.permission", "denied");
+        }
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        stepCount = event.values[0];
+        TextView tv = findViewById(R.id.jalka);
+        tv.setText(Float.toString(stepCount));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, sensorManager.SENSOR_DELAY_NORMAL);
+
+        SharedPreferences prefGet = getSharedPreferences("StepsPref", Activity.MODE_PRIVATE);
+        stepCount = prefGet.getFloat("StepKey", 0);
+
+        TextView tv = findViewById(R.id.jalka);
+        tv.setText(Float.toString(stepCount));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this, sensor);
+
+
+        createStepSave = stepCount;
+        SharedPreferences prefPut = getSharedPreferences("StepsPref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor prefEditor = prefPut.edit();
+        prefEditor.putFloat("StepKey", createStepSave);
+        prefEditor.commit();
+    }
+
+
 }
