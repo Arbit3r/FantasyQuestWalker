d9b32dc7e4c309b2846adeb87da81befe37771b0
nnyeh <emil.munne@gmail.com>
2021-12-15 21:05:48 +0200

    siistimistä

--- app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
+++ app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
@@ -3,13 +3,11 @@ package com.example.fantasyquestwalker;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
@@ -24,7 +22,6 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -38,24 +35,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private float savedStepCount;
     private float createStepSave;
 
+    // ottaa tallennetut askeleet, kävellyt askeleet ja matkan nimen, ja päivittää user interfacen
     public void RefreshUI(){
         SharedPreferences prefGet = getSharedPreferences("StepsPref", Activity.MODE_PRIVATE);
         savedStepCount = prefGet.getFloat("StepKey", 0);
-
         SharedPreferences prefGet2 = getSharedPreferences("StepsPref", Activity.MODE_PRIVATE);
         savedJourney = prefGet2.getInt("indexKey", 0);
 
         TextView tv = findViewById(R.id.number);
         tv.setText(String.format("%.1f", Singleton.getInstance().getMatkat(savedJourney).getMatka()
                 - ((savedStepCount + stepCount) * 0.0007f)));
-
+        
         TextView tv2 = findViewById(R.id.destination);
         tv2.setText(Singleton.getInstance().getMatkat(savedJourney).getNimi());
-
     }
-
-
-
 
     Animation whiteAnimEnabled, whiteAnimDisabled, textAnimEnabled, textAnimDisabled, textAnimLoad;
     LinearLayout white, text;
@@ -68,9 +61,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         text = findViewById(R.id.textbox);
         white = findViewById(R.id.white);
 
-        // asetetaan valikko poissaolevaksi sovellukssa
+        // asetetaan valikko poissaolevaksi sovelluksessa
         white.setVisibility(View.GONE);
-
 
         // loadataan valmiiksi tehdyt animaatiot niiden omista tiedostoista
         new AnimationUtils();
@@ -98,48 +90,43 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
 
         ListView lv = findViewById(R.id.lista);
-
         lv.setAdapter(new ArrayAdapter<Journey>(
                 this,
                 R.layout.centerlist,
                 Singleton.getInstance().getMatkat())
         );
 
+        // listan nappulanpainon jälkeen päivittää user interfacen oikeilla tiedoilla
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-              @Override
-              public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                  //add stuff here
-                  AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                  dialog.setCancelable(false);
-                  dialog.setTitle("Do you want to change quest?");
-                  dialog.setMessage("This will erase all prior progress. " );
-                  dialog.setPositiveButton("Change Quest", new DialogInterface.OnClickListener() {
-                      @Override
-                      public void onClick(DialogInterface dialog, int id) {
-                          createStepSave = 0;
-                          SharedPreferences prefPut = getSharedPreferences("StepsPref", Activity.MODE_PRIVATE);
-                          SharedPreferences.Editor prefEditor = prefPut.edit();
-                          prefEditor.putInt("indexKey", i);
-                          prefEditor.putFloat("StepKey", createStepSave);
-                          prefEditor.commit();
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setCancelable(false);
+                dialog.setTitle("Do you want to change quest?");
+                dialog.setMessage("This will erase all prior progress. " );
+                dialog.setPositiveButton("Change Quest", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        createStepSave = 0;
+                        SharedPreferences prefPut = getSharedPreferences("StepsPref", Activity.MODE_PRIVATE);
+                        SharedPreferences.Editor prefEditor = prefPut.edit();
+                        prefEditor.putInt("indexKey", i);
+                        prefEditor.putFloat("StepKey", createStepSave);
+                        prefEditor.commit();
+                        RefreshUI();
+                    }
+                })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                RefreshUI();
+                            }
+                        });
 
-                          RefreshUI();
-                      }
-                  })
-                          .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                              @Override
-                              public void onClick(DialogInterface dialog, int which) {
-                                  RefreshUI();
-                              }
-                          });
-
-                  final AlertDialog alert = dialog.create();
-                  alert.show();
-
-
-
-              }
-          });
+                final AlertDialog alert = dialog.create();
+                alert.show();
+            }
+        });
 
         // pyytää käyttäjältä lupaa käyttää askelsensoria
         if (ContextCompat.checkSelfPermission(this,
@@ -153,13 +140,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
         stepCount = event.values[0];
         RefreshUI();
     }
@@ -175,12 +159,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         RefreshUI();
     }
 
+    // pitää pitää vaikkei käytä mitään(?)
     @Override
     protected void onPause() {
         super.onPause();
-
     }
 
+    // yhdistää kävellyt askeleet käveltyihin askeleihin ja lopettaa sensorin käytön
     @Override
     protected void onDestroy() {
         super.onDestroy();
