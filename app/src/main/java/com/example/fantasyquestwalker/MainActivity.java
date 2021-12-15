c7f3b7b16845f2c079e4a1b56a186ccb98f2e046
nnyeh <emil.munne@gmail.com>
2021-12-15 20:24:47 +0200

    +kommentit -hajurako

--- app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
+++ app/src/main/java/com/example/fantasyquestwalker/MainActivity.java
@@ -3,11 +3,9 @@ package com.example.fantasyquestwalker;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
@@ -22,7 +20,6 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -36,24 +33,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private float savedStepCount;
     private float createStepSave;
 
+    // ottaa tallennetut askeleet, kävellyt askeleet ja matkan nimen, ja päivittää user interfacen
     public void RefreshUI(){
         SharedPreferences prefGet = getSharedPreferences("StepsPref", Activity.MODE_PRIVATE);
         savedStepCount = prefGet.getFloat("StepKey", 0);
-
         SharedPreferences prefGet2 = getSharedPreferences("StepsPref", Activity.MODE_PRIVATE);
         savedJourney = prefGet2.getInt("indexKey", 0);
-
         TextView tv = findViewById(R.id.number);
         tv.setText(Float.toString(Singleton.getInstance().getMatkat(savedJourney).getMatka()
                 - ((savedStepCount + stepCount) * 0.0007f)));
-
         TextView tv2 = findViewById(R.id.destination);
         tv2.setText(Singleton.getInstance().getMatkat(savedJourney).getNimi());
-
     }
-
-
-
 
     Animation whiteAnimEnabled, whiteAnimDisabled, textAnimEnabled, textAnimDisabled, textAnimLoad;
     LinearLayout white, text;
@@ -68,7 +59,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         // asetetaan valikko poissaolevaksi sovellukssa
         white.setVisibility(View.GONE);
-
 
         // loadataan valmiiksi tehdyt animaatiot niiden omista tiedostoista
         new AnimationUtils();
@@ -96,13 +86,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
 
         ListView lv = findViewById(R.id.lista);
-
         lv.setAdapter(new ArrayAdapter<Journey>(
                 this,
                 R.layout.centerlist,
                 Singleton.getInstance().getMatkat())
         );
 
+        // listan nappulanpainon jälkeen päivittää user interfacen oikeilla tiedoilla
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -113,10 +103,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 prefEditor.putInt("indexKey", i);
                 prefEditor.putFloat("StepKey", createStepSave);
                 prefEditor.commit();
-
                 RefreshUI();
-
-
             }
         });
 
@@ -132,13 +119,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
         stepCount = event.values[0];
         RefreshUI();
     }
@@ -154,12 +138,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         RefreshUI();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-    }
-
+    // yhdistää kävellyt askeleet käveltyihin askeleihin ja lopettaa sensorin käytön
     @Override
     protected void onDestroy() {
         super.onDestroy();
